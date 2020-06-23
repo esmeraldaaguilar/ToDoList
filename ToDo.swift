726b2a9bbdd4d7f00a3136025624578ea3e128d1
Esme Aguilar <eaguilar.4009@gmail.com>
2020-06-23 13:36:21 -0500

    Initial Commit

---
+++ ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Esme Aguilar on 6/22/20.
+//  Copyright © 2020 Esme Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
