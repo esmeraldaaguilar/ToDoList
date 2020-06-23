85503eccc51a75b34a64456ccd5de55dda787c28
Esme Aguilar <eaguilar.4009@gmail.com>
2020-06-23 13:37:25 -0500

    initial commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Esme Aguilar on 6/22/20.
+//  Copyright © 2020 Esme Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var previous = ToDoTableViewController()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
